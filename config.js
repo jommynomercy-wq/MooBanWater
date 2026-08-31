63c71fd2d949ed5e3a024c8d1c43b000206b3ca4
jommynomercy-wq <jommynomercy@gmail.com>
2026-08-31 19:53:19 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.SUPABASE_CONFIG = {
   url: "https://dqeruknurralwkurcqrp.supabase.co",
-  anonKey: "sb_publishable_zxnDkRxVDPg2J4FzUGGZOA_7ncSvA0l"
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRxZXJ1a251cnJhbHdrdXJjcXJwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODgxMTg2MzQsImV4cCI6MjEwMzY5NDYzNH0.OqfQZDR3H6vxqWCgevAly8_9ROdUaDnfcZ9ZyuGpuQI"
 };
